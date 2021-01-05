adc25338ae51b5f8e702f7ef9139a27e7dd19b1c
Hiroto-Nagashima <nh35813219254@gmail.com>
2021-01-05 00:28:12 +0000

    [admin/controller/add_all]

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,28 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'orders/show'
+    get 'orders/update'
+  end
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/update'
+  end
+  namespace :admin do
+    get 'genres/index'
+    get 'genres/create'
+    get 'genres/update'
+    get 'genres/edit'
+  end
+  namespace :admin do
+    get 'items/index'
+    get 'items/new'
+    get 'items/show'
+    get 'items/edit'
+    get 'items/create'
+    get 'items/update'
+  end
  namespace :admin do
     get 'top'=> 'homes#top'
   end
